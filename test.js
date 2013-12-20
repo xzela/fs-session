1d74ce920eaccbf2e04c82df642e98596c888e5f
Zeph Lafassett <zeph@lafassett.com>
2013-12-19 18:05:22 -0800

    converting spaces, removing extra code

--- test.js
+++ test.js
@@ -4,24 +4,24 @@ var connect = require('connect'),
 	FsSession = require('./lib/fs-session')(connect);
 
 var store = new FsSession({dir: 'sessions'});
-store.set('someSessionId', { cookie: {maxAge: 60}}, function(err, file) {
-	if (err) {
-		throw err;
-	} else {
-		console.log(file);
-	}
+// store.set('someSessionId', { cookie: {maxAge: 60}}, function(err, file) {
+// 	if (err) {
+// 		throw err;
+// 	} else {
+// 		console.log(file);
+// 	}
 
-});
-store.get('someSessionId', function(err, file) {
-	if (err) {
-		throw err;
-	} else {
-		console.log(file);
-	}
-});
-// console.log(store);
-// store.destroy('sessss', function(err) {
-// 	console.log(err);
 // });
+// store.get('someSessionId', function(err, file) {
+// 	if (err) {
+// 		throw err;
+// 	} else {
+// 		console.log(file);
+// 	}
+// });
+// console.log(store);
+store.destroy('someSessionId', function(err) {
+	console.log(err);
+});
 // store.purge();
 
