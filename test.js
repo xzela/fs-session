164f8906fbd4762aaacdced9f32ba9804bf41dc0
Zeph Lafassett <zeph@lafassett.com>
2013-12-19 19:46:43 -0800

    starting test suit

--- test.js
+++ test.js
@@ -3,7 +3,28 @@ var connect = require('connect'),
 	assert = require('assert')
 	FsSession = require('./lib/fs-session')(connect);
 
-var store = new FsSession({dir: 'sessions'});
+var store = new FsSession,
+	session_id = 'session_id',
+	cookie = {cookie: {
+		maxAge: 60
+	}};
+
+var onDestroy = function(err, callback) {
+	console.log('verifying destroying session: ' + session_id);
+	assert(!err, '#destroy failed to destroy session');
+};
+
+store.set(session_id, cookie, function(err, data) {
+	var sess = data;
+	// console.log('verifying setting session: ' + session_id);
+	assert(!err, '#set() session failed to serialize');
+	store.expired(session_id, function(err, expired) {
+		assert.equal(expired, false, "#expired session should not be expired already expired");
+	});
+	// store.destroy(session_id, onDestroy);
+});
+
+
 // store.set('someSessionId', { cookie: {maxAge: 60}}, function(err, file) {
 // 	if (err) {
 // 		throw err;
@@ -20,8 +41,8 @@ var store = new FsSession({dir: 'sessions'});
 // 	}
 // });
 // console.log(store);
-store.destroy('someSessionId', function(err) {
-	console.log(err);
-});
+// store.destroy('someSessionId', function(err) {
+// 	console.log(err);
+// });
 // store.purge();
 
