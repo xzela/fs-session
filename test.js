88491fa5f8b5612fab8dd298736bda1cc31a2531
Zeph Lafassett <zeph@lafassett.com>
2013-12-19 17:34:57 -0800

    writing to disk works, getting sort of, but wont distroy when session is expired for some reasonm

--- test.js
+++ test.js
@@ -4,6 +4,15 @@ var connect = require('connect'),
 	FsSession = require('./lib/fs-session')(connect);
 
 var store = new FsSession({dir: 'sessions'});
+// store.set('someSessionId', { cookie: {maxAge: 8987}}, function(err, file) {
+// 	console.log(file);
+// });
+store.get('someSessionId', function(err, file) {
+	console.log(err);
+});
 // console.log(store);
-store.destroy();
+// store.destroy('sessss', function(err) {
+// 	console.log(err);
+// });
+// store.purge();
 
